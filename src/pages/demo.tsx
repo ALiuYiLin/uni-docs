953e7ed8063e619c2383975494327049914f3712
刘一霖 <scliuyilin@163.com>
2026-01-04 22:09:15 +0800

    demo: 自定义页面添加

---
+++ src/pages/demo.tsx
@@ -0,0 +1,10 @@
+import Layout from '@theme/Layout';
+import MDXContent from '../../demo/student.mdx';
+
+export default function StudentsDemoPage() {
+  return (
+    <Layout title="Students Demo">
+      <MDXContent />
+    </Layout>
+  );
+}
